67278ba33134b51b756ecec74ddfe98ac70f1381
Artiom Divak <artten12380@gmail.com>
2021-11-21 17:36:00 +0200

    watchdog

--- ex2/test.py
+++ ex2/test.py
@@ -1,7 +1,23 @@
-import os
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
 
-for currentpath, folders, files in os.walk('.'):
-    for file in files:
-        print(os.path.join( file))
-    for folder in folders:
-        print(os.path.join( folder) + "          1")
+class Event(LoggingEventHandler):
+    def dispatch(self, event):
+        print(event)
+
+
+if __name__ == "__main__":
+    path = sys.argv[1] if len(sys.argv) > 1 else './'
+    event_handler = Event()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
